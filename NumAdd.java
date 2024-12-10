bff54419ca5e77794f516cdcd10b680a6ba42a50
mohammad-mursalin <mursalinlamon@gmail.com>
2024-12-11 01:39:57 +0600

    optimized version

--- NumAdd.java
+++ NumAdd.java
@@ -3,29 +3,48 @@ import java.util.LinkedList;
 class Solution {
     public LinkedList<Integer> addTwoNumbers(LinkedList<Integer> l1, LinkedList<Integer> l2) {
 
+        int n = Math.min(l1.size(), l2.size());
+
+        int sum = 0;
+        int carry = 0;
+
         LinkedList<Integer> list = new LinkedList<>();
 
-        int num1 = 0;
-        int num2 = 0;
+        for(int m = 0; m < n ; m++) {
+            sum = l1.removeFirst() + l2.removeFirst() + carry;
 
-        while(!l1.isEmpty()) {
-            num1 += (int) (l1.removeLast() * Math.pow(10, l1.size()));
+            if(sum > 9) {
+                carry = 1;
+                sum -= 10;
+            }
+            else {
+                carry = 0;
+            }
+            list.addLast(sum);
         }
 
-        while(!l2.isEmpty()) {
-            num2 += (int) (l2.removeLast() * Math.pow(10, l2.size()));
+        while (! l1.isEmpty()) {
+            sum = l1.removeFirst()+ carry;
+            if(sum > 9) {
+                carry = 1;
+                sum -= 10;
+            }
+            else {
+                carry = 0;
+            }
+            list.addLast(sum);
         }
 
-        int result = num1 + num2;
-
-        int divRes = 1;
-        int rem;
-
-        while( divRes > 0 ) {
-            divRes = result/10;
-            rem = result % 10;
-            list.addFirst(rem);
-            result = divRes;
+        while (! l2.isEmpty()) {
+            sum = l2.removeFirst()+ carry;
+            if(sum > 9) {
+                carry = 1;
+                sum -= 10;
+            }
+            else {
+                carry = 0;
+            }
+            list.addLast(sum);
         }
         return list;
     }
@@ -35,12 +54,16 @@ public class NumAdd {
     public static void main(String[] args) {
 
         LinkedList<Integer> list = new LinkedList<>();
-        list.add(3);list.add(4);
+        list.add(3);
+        list.add(5);
+        list.add(6);
+        list.add(2);
+
         LinkedList<Integer> list1 = new LinkedList<>();
-        list1.add(7);list1.add(2);
+        list1.add(4);
+        list1.add(5);
 
         Solution sol = new Solution();
-
         System.out.println(sol.addTwoNumbers(list, list1));
 
 
